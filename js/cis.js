61d17771caeabb1423625b10c71c07284ad154b0
judprog <jud.prog@gmail.com>
2017-08-15 10:34:33 -0400

    cambios para pruebas

--- js/cis.js
+++
@@ -1,9 +0,0 @@
-var Conn = new Conexion();
-let militar = new Militar();
-$(function () {
-    CargarUrl("_bxBuscar", "buscar");
-    CargarUrl("panelperfil", "inc/perfil");
-    CargarUrl("panellista", "inc/lstReembolsos");
-    CargarUrl("panelregistro", "inc/crearReembolso");
-    CargarUrl("modalreembolso","inc/modals");
-});
