d270198bc20162db9d4305dde4c87324c1a868c2
franklin16 <franklinflores16@gmail.com>
2017-09-14 07:41:36 -0400

    modulo de usuario

--- js/usuario.js
+++ js/usuario.js
@@ -30,7 +30,7 @@ class Perfil{
     }
 }
 
-class Firma{
+class FirmaDigital{
     constructor(){
         this.direccionmac = "";
         this.direccionip = "";
@@ -50,16 +50,18 @@ class Usuario{
         this.cargo = "";
         this.telefono = "";
         this.sistema = "";
+        this.FirmaDigital = new FirmaDigital ();
         this.token = "";
         this.Roles = new Roles();
         this.Perfil = new Perfil();
-        this.FirmaDigital = new Firma();
+
+
     }
 
     Obtener(){
        this.cedula = $("#cedula").val();
        this.nombre = $("#nombre").val();
-       this.login = $("#seudonimo").val();
+       this.login = $("#usuario").val();
        this.Roles.descripcion = $("#rolUsuario").val();
        this.Perfil.descripcion = $("#perfilUsuario").val();
        this.cargo = $("#cargo").val();
@@ -67,7 +69,7 @@ class Usuario{
        this.correo = $("#correo").val();
        this.FirmaDigital.direccionip = $("#direccionIp").val();
        this.FirmaDigital.direccionmac = $("#direccionMac").val();
-       this.direccion = $("#direccionUsuario").val();
+       this.direccion = $("#direccionDomicilio").val();
 
        return this;
     }
@@ -79,6 +81,7 @@ class Usuario{
           valores: this.Obtener(),
       });
       requestE.then(function(xhRequest) {
+
         console.log("Obteniendo Datos...");
         console.log(xhRequest);
       });
@@ -87,7 +90,7 @@ class Usuario{
 
 let listaUsuario = null;
 let Conn = new Conexion();
-
+let util = new Utilidad();
 $(function () {
 
     var requestE = CargarAPI({
@@ -104,17 +107,26 @@ $(function () {
 });
 
 
-/*function Salvar(){
+function Salvar(){
   var usuario = new Usuario();
   console.log("Enviando datos para salvar usuario");
   usuario.Salvar();
   console.log("Usuario Salvado!!!");
-*/
+
 }
+
 function llenarLista(){
     $("#cmbListadoUsuario").html("");
-    $.each(listaUsuario,function(){
-        $("#cmbListadoUsuario").append("<option value='"+this.cedula+"'>"+this.nombre+"</option>");
+    listaUsuario.forEach( v => {
+        listaUsuario.forEach(v => { 
+            $("#tblUsuarioCuerpo").append(`
+            <tr><td>${v.sucursal}</td>
+            <td></td>
+            <td></td>
+            <td>${v.nombre}</td>
+            <td>${v.cedula}</td>
+            <td>${v.usuario}</td>
+            <td>${v.estatus}</td></tr>`) });
     });
 }
 
@@ -140,19 +152,29 @@ function cargarUsuario(){
 }
 
 function llenarUsuario(datos){
+
+    $("#fechaCreacion").val(datos.fechacreacion);
+    $("#sistema").val(datos.sistema);
+    $("#cmbGerencia").val(datos.sistema);
+    $("#cmbSucursal").val(datos.sucursal);
+    $("#departamento").val(datos.departamento);
+    $("#areaTrabajo").val(datos.departamento);
     $("#cedula").val(datos.cedula);
-    $("#nombre").val(datos.nombre);
-    $("#seudonimo").val(datos.usuario);
+    $("#apellidos").val(datos.nombre);
+    $("#nombres").val(datos.nombre);
+    $("#situacion").val(datos.situacion);
+    $("#cargo").val(datos.cargo);
     $("#rolUsuario").val(datos.Roles.descripcion);
     $("#perfilUsuario").val(datos.Perfil.descripcion);
-    $("#cargo").val(datos.cargo);
+    $("#funciones").val(datos.funciones);
+    $("#usuario").val(datos.usuario);
+    $("#direccionIp").val(datos.FirmaDigital.direccionip);
+    $("#direccionMac").val(datos.FirmaDigital.Direccionmac);
+    $("#direccionDomicilio").val(datos.direccion);
     $("#telefono").val(datos.telefono);
     $("#correo").val(datos.correo);
-    $("#direccionIp").val(datos.FirmaDigital.DireccionIP);
-    $("#direccionMac").val(datos.FirmaDigital.DireccionMAC);
-    $("#direccionUsuario").val(datos.direccion);
-    $("#fechaCreacion").val(datos.fechacreacion);
-
+    $("#estatus").val(datos.estatus);
+   
 }
 function cargarMenu(){
     var usuario = $("#cmbUsuario option:selected").val();
--- js/utilidad.js
+++ js/utilidad.js
@@ -36,21 +36,25 @@ class Utilidad {
     }
 
     SoloLetras(e) {
-        key = e.keyCode || e.which;
-        tecla = String.fromCharCode(key).toLowerCase();
-        letras = " áéíóúabcdefghijklmnñopqrstuvwxyz";
-        especiales = [8, 37, 39, 46];
+        var key = e.keyCode || e.which;
+        var tecla = String.fromCharCode(key).toLowerCase();
+        var letras = " áéíóúabcdefghijklmnñopqrstuvwxyz";
+        var especiales = [8, 37, 39, 46];
 
-        tecla_especial = false
+        var tecla_especial = false
         for(var i in especiales) {
             if(key == especiales[i]) {
                 tecla_especial = true;
                 break;
             }
         }
-        if(letras.indexOf(tecla) == -1 && !tecla_especial)
+        if(letras.indexOf(tecla) == -1 && !tecla_especial){
+
+
             return false;
-        return true;
+        }
+
+           return true;
     }
 
     SoloNumero(event,elemento,monto) {
